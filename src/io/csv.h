62eeea934d509cd4b7e8e861eb0cc534e61060a2
zoomlogo <maanasyt@gmail.com>
2026-01-08 18:24:08 +0530

    docs

--- src/io/csv.h
+++ src/io/csv.h
@@ -8,16 +8,55 @@
 
 #include "types.h"
 
+/**
+ * @brief CSV File type.
+ */
 typedef struct {
-    FILE *fptr;
-    usize cols;
-    char **header;
+    FILE *fptr; ///< `FILE` pointer to the CSV file.
+    usize cols; ///< Number of columns in the CSV file.
+    char **header; ///< The header of the CSV file.
 } csv_t;
 
+/**
+ * @brief Open a CSV file for writing.
+ *
+ * @param filename The name of the file to (over)write.
+ * @returns A pointer to `csv_t` if everything works, else NULL.
+ */
 csv_t *csv_open(const char *filename);
+/**
+ * @brief Close and free memory of `csv_t`.
+ *
+ * @param csv CSV type.
+ */
 void csv_close(csv_t *csv);
 
+/**
+ * @brief Add a new column.
+ *
+ * Adds a column to the `csv_t` which is written
+ * later with csv_write_header().
+ *
+ * @param csv CSV type.
+ * @param name Column title.
+ * @returns OK on success.
+ */
 error_e csv_add_header(csv_t *csv, const char *name);
+/**
+ * @brief Writes the CSV header.
+ *
+ * @param csv CSV type, with all header added.
+ * @returns OK on success.
+ */
 error_e csv_write_header(csv_t *csv);
 
+/**
+ * @brief Writes a row of data to the CSV file.
+ *
+ * @note `row_data` must be equal to (or more than) the number of columns.
+ *
+ * @param csv CSV type.
+ * @param row_data The data of the entire row.
+ * @returns OK on success.
+ */
 error_e csv_write_row(csv_t *csv, f64 *row_data);
